10e81bb421265a954fa26cd8583050e9cc0b366c
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-16 12:58:31 +0300

    Add project files.

---
+++ G. Palindrome Array/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+bool isPalindrome = true;
+for (int i = 0; i < n / 2; i++)
+{
+    if (arr[i] != arr[n - i - 1])
+    {
+        isPalindrome = false;
+        break;
+    }
+}
+
+Console.WriteLine(isPalindrome ? "YES" : "NO");
